d41e9fdd2d6f06e56e8ad085a99b515ced1b03ab
Casey Brown <cbrowncodes@gmail.com>
2018-11-04 17:48:30 -0600

    Update GUI.java

--- JIVE/src/jive/java/GUI.java
+++ JIVE/src/jive/java/GUI.java
@@ -110,14 +110,6 @@ public class GUI
 		project.saveAs(newFileName,newFileName.substring(newFileName.lastIndexOf(".")));
 		
 	}
-	public String getFileExtension(File file) {
-    		String name = file.getName();
-    		int lastIndexOf = name.lastIndexOf(".");
-    		if (lastIndexOf == -1) {
-        		return ""; // empty extension
-    		}
-    		return name.substring(lastIndexOf);
-	}
 	//TODO:
 	@FXML void helpAction() 
 	{
